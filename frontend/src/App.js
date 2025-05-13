26fdc25cbd4411f3f9061fc765b21bbb69e43c26
amT9x <hoangvantung155@gmail.com>
2025-05-13 16:47:42 +0700

    init react with ui in folder template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,242 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="grid-container">
+            {/* <!-- header --> */}
+            <header class="row">
+                <div>
+                    <a class="brand" href="index.html">Ecommerce</a>
+                </div>
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="singin.html">Sign In</a>
+                </div>
+            </header>
+            {/* <!-- main --> */}
+            <main>
+                <div class="row centertunghv">
+                    {/* <!-- Card --> */}
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* <!-- rating --> */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                            </div>
+                            {/* <!-- price --> */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* <!-- rating --> */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                            </div>
+                            {/* <!-- price --> */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* <!-- rating --> */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                            </div>
+                            {/* <!-- price --> */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* <!-- rating --> */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                            </div>
+                            {/* <!-- price --> */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* <!-- rating --> */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                            </div>
+                            {/* price */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="product.html">
+                            {/* image size: 680px by 830px */}
+                            <img class="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div class="card-body">
+                            <a href="product.html">
+                                <h2>Women White Shirts</h2>
+                            </a>
+                            {/* Rating */}
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i class="fa fa-star-half-o"></i>
+                                </span>
+                            </div>
+                            {/* Price */}
+                            <div class="price">
+                                $120
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            {/* Footer */}
+            <footer class="row centertunghv">All right reserved</footer>
+        </div>
   );
 }
 
