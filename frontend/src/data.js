5d87095e2d0026997b202c51d04409dde6ef1172
amT9x <hoangvantung155@gmail.com>
2025-05-13 17:32:23 +0700

    kind of products

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data = {
+    "products": [
+        {
+            "_id": 1,
+            "name": "Women 1",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 1",
+            "price": 29.99,
+            "image": "/images/p1.jpg"
+        },
+        {
+            "_id": 2,
+            "name": "Women 2",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 2",
+            "price": 29.99,
+            "image": "/images/p2.jpg"
+        },
+        {
+            "_id": 3,
+            "name": "Women 3",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 3",
+            "price": 29.99,
+            "image": "/images/p3.jpg"
+        },
+        {
+            "_id": 4,
+            "name": "Women 4",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 4",
+            "price": 29.99,
+            "image": "/images/p4.jpg"
+        },
+        {
+            "_id": 5,
+            "name": "Women 5",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 5",
+            "price": 29.99,
+            "image": "/images/p5.jpg"
+        },
+        {
+            "_id": 6,
+            "name": "Women 6",
+            "category": "Dresses",
+            "brand": "Unknown",
+            "rating": 4.5,
+            "numReviews": 10,
+            "description": "Description for Women 6",
+            "price": 29.99,
+            "image": "/images/p6.jpg"
+        },
+    ]
+}
