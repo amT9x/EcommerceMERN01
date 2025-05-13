9e989d78fbfc3340592b53791e0237a93647b31a
amT9x <hoangvantung155@gmail.com>
2025-05-13 21:42:20 +0700

    clean code: use component for Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import data from './data';
+import Product from './components/Product';
 
 function App() {
   return (
@@ -19,40 +20,7 @@ function App() {
           {/* <!-- Card --> */}
           {
             data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`product/${product._id}`}>
-                  {/* image size: 680px by 830px */}
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                  <a href={`product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  {/* <!-- rating --> */}
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  {/* <!-- price --> */}
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} pros={{ product }} />
             ))
           }
         </div>
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Product({ pros }) {
+    const { product } = pros;
+    return(
+        <div key={product._id} className="card">
+                <a href={`product/${product._id}`}>
+                  {/* image size: 680px by 830px */}
+                  <img className="medium" src={product.image} alt={product.name} />
+                </a>
+                <div className="card-body">
+                  <a href={`product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  {/* <!-- rating --> */}
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  {/* <!-- price --> */}
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+    );
+}
